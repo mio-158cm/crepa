007c4b1a950910a71db52ea4917ce2ce13ac0f2e
mio-158cm <hayakawan18@gmail.com>
2019-06-03 19:34:12 +0900

    update-controller

--- myapp/app/controllers/users_controller.rb
+++ myapp/app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
+
   def index
+    @users = User.where('name LIKE(?)', "%#{params[:name]}%" ).where.not(id: current_user)
+    respond_to do |format|
+
+      format.json
+    end
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+   params.require(:user).permit(:name, :email)
+  end
+
+
+  def edit
   end
 
   def update
   end
 
-  def edit
-  end
 end
